cb90b0098b3ba5332e5c984a1cbb0aacd25292c2
NidhinJohny77 <nidhinjohny77@gmail.com>
2023-01-03 14:03:53 +0000

    Splitted home component into location-component,user-component and dashboard-component

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,13 +1,24 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../shared/guards';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path:'home',
     component:HomeComponent,
-    canActivate:[AuthGuard]
+    canActivate:[AuthGuard],
+    children:[
+      {
+        path:'',
+        component:DashboardComponent
+      },
+      {
+        path:'dashboard',
+        component:DashboardComponent
+      }
+    ]
   }
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { PropertyListingComponent } from './property-listing/property-listing.component';
+import { PropertyDetailComponent } from './property-detail/property-detail.component';
+import { LocationComponent } from './location/location.component';
+import { UserComponent } from './user/user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    PropertyListingComponent,
+    PropertyDetailComponent,
+    LocationComponent,
+    UserComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
