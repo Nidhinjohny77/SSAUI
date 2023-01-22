de8fc110f0819d9ecf73b80bb104e28c49ca9040
NidhinJohny77 <nidhinjohny77@gmail.com>
2023-01-22 22:51:55 +0000

    Fixed view booking on the detail page.

--- src/app/home/property-detail/property-detail.component.ts
+++ src/app/home/property-detail/property-detail.component.ts
@@ -22,6 +22,17 @@ export class PropertyDetailComponent implements OnInit {
   furnishTypes:Array<MultiSelectData>;
   propertyTypes:Array<MultiSelectData>;
   times:Array<string>;
+  bookingMessage:string|undefined;
+  isBookingDisabled:boolean=false;
+  isFromTimeSelectionDisabled=false;
+  isToTimeSelectionDisabled=false;
+  isFromDateSelectionDisabled=false;
+  isToDateSelectionDisabled=false;
+  fromDate:string|undefined;
+  fromTime:string|undefined="Choose Time";
+  toDate:string|undefined;
+  toTime:string|undefined="Choose Time";
+  isBookingSuccess:boolean=false;
 
   constructor(private propertyService:PropertyService,private locationService:Location,
     private masterService:MasterService) { 
@@ -36,17 +47,48 @@ export class PropertyDetailComponent implements OnInit {
     if(state){
       this.property= new Property(state);
     }
-    initializePropertyDetails();
     this.masterService.getFurnishTypes().subscribe(data=>{
       this.furnishTypes=data;
     });
     this.masterService.getPropertyTypes().subscribe(data=>{
       this.propertyTypes=data;
     });
+    initializePropertyDetails();
+  }
+
+  onBook(){
+    if(!this.fromDate || (this.fromTime=="Choose Time") || !this.toDate ||(this.toTime=="Choose Time")){
+      this.isBookingSuccess=false;
+      this.bookingMessage="Selected input date is not correct."
+    }
+    else{
+      this.isBookingSuccess=true;
+      this.bookingMessage="The viewing has been booked."
+      this.isBookingDisabled=true;
+      this.isFromTimeSelectionDisabled=true;
+      this.isToTimeSelectionDisabled=true;
+      this.isFromDateSelectionDisabled=true;
+      this.isToDateSelectionDisabled=true;
+    }
+  }
+
+  onModalClose(){
+    this.fromDate=undefined;
+    this.fromTime="Choose Time";
+    this.toDate=undefined;
+    this.toTime="Choose Time";
+    this.isBookingSuccess=false;
+    this.bookingMessage=undefined;
+    this.isBookingDisabled=false; 
+    this.isFromTimeSelectionDisabled=false;
+    this.isToTimeSelectionDisabled=false;
+    this.isFromDateSelectionDisabled=false;
+    this.isToDateSelectionDisabled=false;
   }
 
   private getTimeData(start:number,end:number):Array<string>{
     let result=new Array<string>();
+    result.push("Choose Time");
     for(var i=start;i<=end;i++){
       var timeDetails=i.toString()+":"+"00";
       result.push(timeDetails);
